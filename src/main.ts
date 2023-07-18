93e0e4998a3d7f1aa0d1eb19d75935ed58ed75d6
beyman.lopez <beyman1@outlook.com>
2023-07-18 15:50:30 -0500

    prueba de componentes de google material design y estructuracion de libreria de componentes dinamicos.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import '@material/web/button/outlined-button';
+import '@material/web/checkbox/checkbox';
+import '@material/web/button/filled-button';
+import '@material/web/button/elevated-button';
+import '@material/web/button/outlined-button';
+import '@material/web/button/text-button';
+import '@material/web/button/tonal-button';
+import '@material/web/select/outlined-select';
+import '@material/web/select/filled-select';
+import '@material/web/select/select-option';
+import '@material/web/switch/switch';
+import '@material/web/slider/slider';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
---
+++ src/shared/dynamic-components/index.ts
@@ -0,0 +1,4 @@
+//MODULE
+export * from './dynamic-form/dynamic-form.module';
+
+export * from './dynamic-form/components/dynamic-form/dynamic-form.component';
